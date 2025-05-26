2c5916aa0584071bd75816a8207c592efaecb556
Paumaro92 <paumr92@hotmail.es>
2025-05-26 12:57:49 +0200

    Ejercicio 2, añadido métodos mágicos.

--- perfil.php
+++ perfil.php
@@ -1,18 +1,38 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['usuari'])) {
+if (!isset($_SESSION['usuari']) || !isset($_SESSION['email'])) {
     header("Location: s1_06_exercici1.html");
     exit();
 }
+
+class Usuari {
+    public $nom;
+    public $email;
+
+    public function __construct($nom, $email) {
+        $this->nom = $nom;
+        $this->email = $email;
+    }
+
+    public function __toString() {
+        return "<p>Usuari: " . htmlspecialchars($this->nom) . "</p>" .
+               "<p>E-mail: " . htmlspecialchars($this->email) . "</p>";
+    }
+
+ 
+}
+
+$usuari = new Usuari($_SESSION['usuari'], $_SESSION['email']);
 ?>
 
 <!DOCTYPE html>
 <html>
 <body>
     <h1>Perfil de l'usuari</h1>
-    <p>Usuari: <?php echo htmlspecialchars($_SESSION['usuari']); ?></p>
-    <p>E-mail: <?php echo htmlspecialchars($_SESSION['email']); ?></p>
+    <?php
+        echo $usuari;
+    ?>
     <p><a href="s1_06_exercici1.html">Torna al formulari</a></p>
 </body>
 </html>
